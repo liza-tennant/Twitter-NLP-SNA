78eb2b72e1eca04ad8bc3306967c5a8bfa5e6e0c
Elizaveta Karmannaya <51918455+Liza-Karmannaya@users.noreply.github.com>
2020-04-06 23:13:26 +0100

    Update and rename data_collection_6_random_sampler.py to data_collection_5_random_sampler.py

--- data_collection_5_random_sampler.py
+++ data_collection_5_random_sampler.py
@@ -1,10 +1,12 @@
 """
-data_collection_6_random_sampler.py 
+data_collection_5_random_sampler.py 
 
-This code goes through all followers in  filters them by activity
+This code takes an input csv (largest_cc_LEFT_nodelist_filtered.csv or 
+largest_cc_RIGHTT_nodelist_filtered.csv)with user_ids (nodes from the graph), 
+randomly samples 100,000 from them and saves the sample as sample1_LEFT.csv or sample1_RIGHT.csv. 
 
-1 - draw random samples of 100000 from left & right CC nodelists (user_id lists)
-
+1 - draw random samples of 100000 from left CC nodelist (user_id lists) & save as sample1_<...>.csv 
+2 - REPEAT FOR largest_cc_RIGHT_nodelist_filtered.csv
 
 @author: lizakarmannaya
 """
@@ -106,88 +108,3 @@ writeToFile(filtered, remainingList) #remainingList defined above
 #d = pd.read_csv('largest_cc_LEFT_nodelist.csv', header=None)
 #d[0:10]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-######## 4 - drafts - try to collect 200 most recent tweets from one account #################
-
-
-#from O'Reilly books - example for NASA 
-nasa_tweets = api.user_timeline(user_id=11348282, count=200, tweet_mode='extended') #screen_name='nasa'
-for tweet in nasa_tweets:
-    print(f'{tweet.user.screen_name}:{tweet.full_text}\n')
-#when 'extended' mode is specified, all tweets are returned with 'full_text' as the relevant text field 
-type(nasa_tweets) #tweepy.models.ResultSet
-
-
-len('NASA:On Feb. 28, @NASAEarth satellites observed reduced levels of atmospheric nitrogen dioxide over China since the coronavirus outbreak. But measurable change in one pollutant does not mean air quality is suddenly healthy. Heres why: https://t.co/2Z5juBUeEO https://t.co/97PXQBcKyh') #282
-
-print(nasa_tweets[0]._json.keys())
-print(nasa_tweets[4].truncated)
-print(len(nasa_tweets[4].text)) #140
-print(nasa_tweets[4].text[-20:])
-print(len(nasa_tweets[4].retweeted_status.text)) #140
-
-
-# Extended Tweets are identified with a root-level "truncated" boolean. 
-# When true ("truncated": true), the "extended_tweet" fields should be parsed instead of the root-level fields.
-status = api.statuses_lookup(id_ = ['1240342876636229632'], tweet_mode='extended')
-status[0]._json.keys()
-status[0].truncated #False if we use 'extended' mode above
-status[0].full_text
-
-
-nasa_tweets[0].user._json.keys()
-len(nasa_tweets) #200
-
-
-#creatng csv for test database 
-list_of_ids = [11348282, 17471979]
-#list_of_ids.to_csv('sample_draft.csv') #can't turn list to csv
-
-largest_cc_LEFT_nodelist = pd.read_csv('largest_cc_LEFT_nodelist.csv', header=None, index_col=0)
-largest_cc_LEFT_nodelist[0:5].to_csv('sample_draft.csv')
-largest_cc_LEFT_nodelist[0:5]
